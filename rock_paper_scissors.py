4015544dfd2e779675fc6160ea81f36d9e318d30
toomeygm <114159795+toomeygm@users.noreply.github.com>
2022-11-14 22:23:12 +0100

    More practice

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -14,6 +14,27 @@ def player1_wins(player1_move, player2_move):
     """
     if player1_move == ROCK and player2_move == SCISSORS:
         return True
+    if player1_move == PAPER and player2_move == ROCK:
+        return True
+    if player1_move == SCISSORS and player2_move == PAPER:
+        return True
+
+    # TODO: finish the rest of this conditional logic
+
+    else:
+        return False 
+
+def player2_wins(player2_move, player1_move):
+    """
+    Returns True if player2_move beats player1_move, or False if not.
+    Note that this returns False in case of a tie!
+    """
+    if player2_move == ROCK and player1_move == SCISSORS:
+        return True
+    if player2_move == PAPER and player1_move == ROCK:
+        return True
+    if player2_move == SCISSORS and player1_move == PAPER:
+        return True
 
     # TODO: finish the rest of this conditional logic
 
@@ -27,6 +48,15 @@ def is_valid_move(move):
     """
     # TODO: implement this function body
 
+    if move == ROCK:
+        return True
+    if move == PAPER:
+        return True
+    if move == SCISSORS:
+        return True
+    else:
+        return False 
+
 
 def play_rock_paper_scissors():
     # This randomly selects one of the options in POSSIBLE_MOVES
@@ -37,14 +67,16 @@ def play_rock_paper_scissors():
         print(f"choose from: {POSSIBLE_MOVES}")
         user_move = input('enter your move: ')
         if is_valid_move(user_move):
-            pass  # TODO: replace `pass` with the necessary logic
+            break  # TODO: replace `pass` with the necessary logic
         else:
             print('invalid input, try again')
 
     print(f"computer's move: {computer_move}")
-    if ...:  # TODO: replace `...` with the correct condition
+    if user_move == computer_move:  # TODO: replace `...` with the correct condition
         print("it's a tie!")
     elif player1_wins(user_move, computer_move):
+        print('you win! 🏆🏆🏆')
+    elif player2_wins(user_move, computer_move):
         print('you win! 🏆🏆🏆')
     else:
         print('computer wins! 😭😭😭')
